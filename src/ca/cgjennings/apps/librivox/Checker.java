6b044d275006a055926f1e04ad25aa129589c0c0
Christopher Jennings <25697451+CGJennings@users.noreply.github.com>
2021-04-30 20:55:28 -0400

    update version

--- src/ca/cgjennings/apps/librivox/Checker.java
+++ src/ca/cgjennings/apps/librivox/Checker.java
@@ -79,7 +79,7 @@ public final class Checker extends javax.swing.JFrame {
     /**
      * Current version number.
      */
-    public static final String VERSION = "1.0.97";
+    public static final String VERSION = "1.1";
 
     /**
      * Creates a new Checker application instance.
